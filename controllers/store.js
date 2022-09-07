29ad2c5669b7abc822b7f4b2688f468821d3a048
Phil Neibaur <pneibaur@Phils-MacBook-Pro.local>
2022-09-07 10:10:14 -0600

    adds store.js controller

---
+++ controllers/store.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const storeRouter = express.Router();
+const Products = require("../models/products.js");
+
+
+////// ROUTES
+// root & seed ------- COMMENTING OUT as I currently don't want to go to this route right now. 
+// const seedData = require("./models/seedData.js")
+// app.get("/products/seed", (req, res)=>{
+//     Products.deleteMany({}, (error, allProducts)=>{})
+//     Products.create(seedData, (error, data)=>{
+//         res.redirect("/products")
+//     })
+// })
+
+// Index
+storeRouter.get("/", (req, res)=>{
+    Products.find({}, (error, allProducts)=>{
+        res.render("index.ejs", {
+            allProducts: allProducts,
+            tabTitle: "All Items"
+        })
+    })
+})
+
+// New
+storeRouter.get("/new", (req, res)=>{
+    res.render("new.ejs", {
+        tabTitle: "New Item"
+    })
+})
+// Destroy
+storeRouter.delete("/:id", (req, res)=>{
+    Products.findByIdAndDelete(req.params.id, (error, foundProduct)=>{
+        res.redirect("/products")
+    })
+})  
+
+// Update
+storeRouter.put("/:id", (req, res)=>{
+    Products.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new:true},
+        (error, updatedItem) =>{
+            res.redirect(`/products/${req.params.id}`)
+        }
+    )
+})
+// Create
+storeRouter.post("/", (req, res)=>{
+    Products.create(req.body, (error, newProduct)=>{
+        res.redirect("/products")
+    })
+})
+
+// Edit 
+// ///////////////I attemted to do this by memory, but couldn't make it work and ran out of time. 
+storeRouter.get("/:id/edit", (req, res)=>{
+    Products.findById(req.params.id, (error, foundItem)=>{
+        res.redirect("/products")
+    })
+})
+// Show
+storeRouter.get("/:id", (req, res)=>{
+    Products.findById(req.params.id, (error, foundProduct) =>{
+        res.render("show.ejs", {
+            showProduct: foundProduct,
+            tabTitle: "Item View",
+        })
+    })
+})
+
+module.exports = storeRouter
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 ////// DEPENDENCIES
 const express = require("express")
+const mongoose = require("mongoose")
 const methodOverride = require("method-override")
+const storeController = require("./controllers/store.js")
 const app = express()
 require("dotenv").config()
 const DATABASE_URL = process.env.DATABASE_URL
-const Products = require("./models/products.js")
-const seedData = require("./models/seedData.js")
-const mongoose = require("mongoose")
 const DB = mongoose.connection
-// Connect to Database
+
+// Database Config
 mongoose.connect(DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-// error and success messages
+    // error and success messages
 DB.on("error", (err)=> console.log(err.message + ": is mongoose up?"))
 DB.on("connected", () => console.log("Mongoose connected"))
 DB.on("disconnected", ()=> console.log("Mongoose disconnected"))
@@ -21,71 +21,8 @@ DB.on("disconnected", ()=> console.log("Mongoose disconnected"))
 ////// MIDDLEWARE
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride("_method"))
-
-////// ROUTES
-// root & seed ------- COMMENTING OUT as I currently don't want to go to this route right now. 
-// app.get("/", (req, res)=> res.send("<a href='/products'>Go to Products</a>"))
-// app.get("/products/seed", (req, res)=>{
-//     Products.deleteMany({}, (error, allProducts)=>{})
-//     Products.create(seedData, (error, data)=>{
-//         res.redirect("/products")
-//     })
-// })
-
-// Index
-app.get("/products", (req, res)=>{
-    Products.find({}, (error, allProducts)=>{
-        res.render("index.ejs", {
-            allProducts: allProducts,
-            tabTitle: "All Items"
-        })
-    })
-})
-
-// New
-app.get("/products/new", (req, res)=>{
-    res.render("new.ejs", {
-        tabTitle: "New Item"
-    })
-})
-// Destroy
-app.delete("/products/:id", (req, res)=>{
-    Products.findByIdAndDelete(req.params.id, (error, foundProduct)=>{
-        res.redirect("/products")
-    })
-})
-
-// Update
-// Create
-app.post("/products", (req, res)=>{
-    Products.create(req.body, (error, newProduct)=>{
-        res.redirect("/products")
-    })
-})
-
-// Edit 
-// ///////////////I attemted to do this by memory, but couldn't make it work and ran out of time. 
-// app.put("/products/:id", (req, res)=>{
-//     Products.findByIdAndUpdate(
-//         req.params.id,
-//         req.body,
-//         {new:true},
-//         (error, updatedItem) =>{
-//             res.render("edit.ejs")
-//         }
-//     )
-// })
-
-// Show
-app.get("/products/:id", (req, res)=>{
-    Products.findById(req.params.id, (error, foundProduct) =>{
-        res.render("show.ejs", {
-            showProduct: foundProduct,
-            tabTitle: "Item View",
-        })
-    })
-})
+app.use("/products", storeController)
 
 ////// LISTENER
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=> console.log("connected on port", PORT))
