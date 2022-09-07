caffb02841324f4ca5ab5e75a1434d5ab7bd68d7
Phil Neibaur <pneibaur@Phils-MacBook-Pro.local>
2022-09-06 21:42:49 -0600

    fixes /new error. adds preliminary styling

--- server.js
+++ server.js
@@ -17,9 +17,11 @@ mongoose.connect(DATABASE_URL, {
 DB.on("error", (err)=> console.log(err.message + ": is mongoose up?"))
 DB.on("connected", () => console.log("Mongoose connected"))
 DB.on("disconnected", ()=> console.log("Mongoose disconnected"))
+
 ////// MIDDLEWARE
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride("_method"))
+
 ////// ROUTES
 // root & seed
 app.get("/", (req, res)=> res.send("<a href='/products'>Go to Products</a>"))
@@ -29,6 +31,7 @@ app.get("/products/seed", (req, res)=>{
         res.redirect("/products")
     })
 })
+
 // Index
 app.get("/products", (req, res)=>{
     Products.find({}, (error, allProducts)=>{
@@ -37,22 +40,48 @@ app.get("/products", (req, res)=>{
         })
     })
 })
+
 // New
 app.get("/products/new", (req, res)=>{
     res.render("new.ejs")
 })
 // Destroy
+app.delete("/products/:id", (req, res)=>{
+    Products.findByIdAndDelete(req.params.id, (error, foundProduct)=>{
+        res.redirect("/products")
+    })
+})
+
 // Update
 // Create
-// Edit
+app.post("/products", (req, res)=>{
+    Products.create(req.body, (error, newProduct)=>{
+        res.redirect("/products")
+    })
+})
+
+// Edit 
+// ///////////////I attemted to do this by memory, but couldn't make it work and ran out of time. 
+// app.put("/products/:id", (req, res)=>{
+//     Products.findByIdAndUpdate(
+//         req.params.id,
+//         req.body,
+//         {new:true},
+//         (error, updatedItem) =>{
+//             res.render("edit.ejs")
+//         }
+//     )
+// })
+
 // Show
 app.get("/products/:id", (req, res)=>{
     Products.findById(req.params.id, (error, foundProduct) =>{
         res.render("show.ejs", {
-            foundProduct: foundProduct,
+            showProduct: foundProduct,
         })
     })
 })
+
 ////// LISTENER
 const PORT = process.env.PORT
 app.listen(PORT, ()=> console.log("connected on port", PORT))
